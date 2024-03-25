823cfd1003fdff79115023a0081e166bf2054da4
annie-hola <anhanhanh151199@gmail.com>
2024-03-25 22:33:41 +0700

    update

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ imgSize = 300
 folder = "Data/C"
 counter = 0
 labels = ["A", "B", "C"]
+while True:
+    success, img = cap.read()
+    imgOutput = img.copy()
+    hands, img = detector.findHands(img)
