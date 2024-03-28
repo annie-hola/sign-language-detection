b4f7ec75b0f2b34091e84f5e2b8eb98fb85e2378
annie-hola <anhanhanh151199@gmail.com>
2024-03-28 23:11:48 +0700

    update

--- test.py
+++ test.py
@@ -3,20 +3,21 @@ from cvzone.HandTrackingModule import HandDetector
 from cvzone.ClassificationModule import Classifier
 import numpy as np
 import math
-cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=1)
+cap = cv2.VideoCapture(0) #open capture video
+detector = HandDetector(maxHands=1) #detect hand
 classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
 offset = 20
 imgSize = 300
 folder = "Data/C"
 counter = 0
 labels = ["A", "B", "C"]
+
 while True:
     success, img = cap.read()
     imgOutput = img.copy()
     hands, img = detector.findHands(img)
     if hands:
-        hand = hands[0]
+        hand = hands[0] # only take one hand
         x, y, w, h = hand['bbox']
         imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
         imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
@@ -44,6 +45,8 @@ while True:
         cv2.putText(imgOutput, labels[index], (x, y -26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
         cv2.rectangle(imgOutput, (x-offset, y-offset),
                       (x + w+offset, y + h+offset), (255, 0, 255), 4)
+        
+        #put image crop to image white => to adjust hcn image 
         cv2.imshow("ImageCrop", imgCrop)
         cv2.imshow("ImageWhite", imgWhite)
     cv2.imshow("Image", imgOutput)
